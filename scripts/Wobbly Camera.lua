c527ad6bb949f07641f274a8c533f1a23f8bd5b2
GNamimates <gnamimates188@gmail.com>
2023-06-17 15:34:57 +0800

    Micro Scripts additions

---
+++ scripts/Wobbly Camera.lua
@@ -0,0 +1,34 @@
+local mac = require("libraries.macroScriptLib"):newScript()
+
+local loc_vel = vectors.vec3()
+
+local rest = vectors.vec3()
+local lstate = vectors.vec3()
+local state = vectors.vec3()
+local vel = vectors.vec3()
+
+mac.START:register(function ()
+
+end)
+
+mac.TICK:register(function ()
+    lstate = state:copy()
+    loc_vel = (player:getVelocity():augmented() * matrices.mat4():rotateY(player:getRot().y)).xyz
+    state = state + vel
+    vel = vel * 0.8 - (state-rest) * 0.5
+    renderer:setCrosshairOffset()
+    rest = loc_vel
+end)
+
+mac.FRAME:register(function (delta)
+    local lv = math.lerp(lstate,state,delta)
+   renderer:offsetCameraRot(lv.z*-15,0,lv.x*-15)
+   renderer:offsetCameraPivot(0,lv.y*0.5,0)
+end)
+
+mac.END:register(function ()
+    renderer:offsetCameraRot(0,0,0)
+    renderer:offsetCameraPivot(0,0,0)
+end)
+
+return mac
