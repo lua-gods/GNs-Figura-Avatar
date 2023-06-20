5b6bdd04e3f5a64f87570539b446be992f716216
GN <gnamimates188@gmail.com>
2023-06-20 22:09:58 +0800

    right click more checks

--- scripts/right click ride.lua
+++ scripts/right click ride.lua
@@ -3,7 +3,12 @@ local mac = require("libraries.macroScriptLib"):newScript()
 keybinds:newKeybind("GNERRS",keybinds:getVanillaKey("key.use")).press = function ()
    if mac.is_active and player:isLoaded() then
       local target = player:getTargetedEntity()
-      host:sendChatCommand("ride "..player:getUUID().." mount "..target:getUUID())
+      if target then
+         if player:getVehicle() then
+            host:sendChatCommand("ride "..player:getUUID().." dismount")
+         end
+         host:sendChatCommand("ride "..player:getUUID().." mount "..target:getUUID())
+      end
    end
 end
 
